26d5a0e32ff927dcae1991cb60abbfd3cf25428c
Vivek Nariya 🚀 <vhnariya@gmail.com>
2024-07-27 17:07:01 +0530

    second case : with one number

--- string-calculator-tdd.js
+++ string-calculator-tdd.js
@@ -1,5 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
+
+  return Number(numbers);
 }
 
 module.exports = add;
--- string-calculator-tdd.test.js
+++ string-calculator-tdd.test.js
@@ -3,3 +3,7 @@ const add = require("./string-calculator-tdd");
 test("should return 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("should return result for a one number", () => {
+  expect(add("9")).toBe(9);
+});
